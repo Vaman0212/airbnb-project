2b5922917ddb5048c0792a5809cff7bd6184bb50
Namav404 <sec.acc901@gmail.com>
2024-06-09 18:26:40 +0530

    Created EmptyState and ListingCard component. Added image view to the ListingCard component

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -53,7 +53,7 @@ const RegisterModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="Welcome to Airbnb" subtitle="Create an account!" center />
+      <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
       <Input
         id="email"
         label="Email"
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import { getListings } from "./actions/getListings";
 import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length == 0) {
+    return <EmptyState showReset />;
+  }
   return (
     <Container>
       <div
@@ -16,7 +26,15 @@ export default function Home() {
         gap-8
         "
       >
-        <div>My future listings</div>
+        {listings.map((listing: any) => {
+          return (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          );
+        })}
       </div>
     </Container>
   );
